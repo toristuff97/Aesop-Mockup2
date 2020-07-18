012a779167aa62890b61fc9acbda74179091ff6e
Omari Brown <omaris.brown@gmail.com>
2020-07-18 16:44:02 -0400

    added hero image and styling -read description
    Having problems linking folders, so I have been putting everything used into components directory until I figure out how to link outside of the current folder.

--- aesop_app/src/Components/TitleBanner.js
+++ aesop_app/src/Components/TitleBanner.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import './TitleBanner.css';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+// import assets from 'src'
 
 class TitleBanner extends React.Component {
     render() {
         return (
             <div>
                 <div className='banner'>
-                    {/* <div className='left-head'>
-                        <h2>Aesop</h2>
-                    </div>
-                    <div className='right-head'>
-                        <h2>Formulations for skin, hair, home, and body</h2>
-                        <h4>Created with sensory pleasure in mind.</h4>
-                    </div> */}
-                    <Container>
+                    <Container className='hero-text'>
                         <Row>
                             <Col sm={4}>
                                 <h2>Aesop</h2>
                             </Col>
                             <Col sm={8}>
                                 <h2>Formulations for skin, hair, home, and body</h2>
+                                <h4>Created with sensory pleasure in mind.</h4>
                             </Col>
                         </Row>
                     </Container>
