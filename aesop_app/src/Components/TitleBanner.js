a1abec73a367a5d970b1f4b146d40457060db204
Omari Brown <omaris.brown@gmail.com>
2020-07-18 16:09:26 -0400

    imported bootstrap and added css file

--- aesop_app/src/Components/TitleBanner.js
+++ aesop_app/src/Components/TitleBanner.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React from 'react';
+import './TitleBanner.css';
+import Container from 'react-bootstrap/Container';
 
 class TitleBanner extends React.Component {
     render() {
         return (
             <div>
                 <div className='banner'>
-                    <div className='left-head'>
+                    {/* <div className='left-head'>
                         <h2>Aesop</h2>
                     </div>
                     <div className='right-head'>
                         <h2>Formulations for skin, hair, home, and body</h2>
                         <h4>Created with sensory pleasure in mind.</h4>
-                    </div>
+                    </div> */}
+                    <Container>
+                        <Row>
+                            <Col sm={4}>
+                                <h2>Aesop</h2>
+                            </Col>
+                            <Col sm={8}>
+                                <h2>Formulations for skin, hair, home, and body</h2>
+                            </Col>
+                        </Row>
+                    </Container>
                 </div>
+                
             </div>
         )
     }
