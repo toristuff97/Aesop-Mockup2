3cc58edc4bdba6199d5027c2d2a8cfa175099433
Omari Brown <omaris.brown@gmail.com>
2020-07-18 15:27:57 -0400

    added text to header

---
+++ aesop_app/src/Components/TitleBanner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class TitleBanner extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className='banner'>
+                    <div className='left-head'>
+                        <h2>Aesop</h2>
+                    </div>
+                    <div className='right-head'>
+                        <h2>Formulations for skin, hair, home, and body</h2>
+                        <h4>Created with sensory pleasure in mind.</h4>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TitleBanner;
