7974388b271423247aa0abfdab9f1cc3fc4d1157
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-07-19 19:53:44 -0400

    added HandsAndBody to app.js

--- aesop_app/src/App.js
+++ aesop_app/src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Fragrance from './Components/Fragrance';
 import Carousel from 'bootstrap';
+import HandsAndBody from './Components/HandsAndBody';
 
 
 function App() {
   return (
     <div className="App">
       <Fragrance />
+      <HandsAndBody />
     </div>
   );
 }
