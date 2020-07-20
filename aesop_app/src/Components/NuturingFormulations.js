04367dd2a66c562b7afdbe9eeafad2eddcecdccf
Samira Esquina <samiraesquina@gmail.com>
2020-07-20 15:53:08 -0400

    Fixed NF css style

--- aesop_app/src/Components/NuturingFormulations.js
+++ aesop_app/src/Components/NuturingFormulations.js
@@ -1,12 +1,11 @@
-import './styles/NurturingFormulations';
+import "./styles/NurturingFormulations";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import Logo from '../assets/images/for_all_products/Aesop-SkinParsley-Seed-Facial-Cleanser-100mL-large.png';
-import Logo1 from '../assets/images/for_all_products/Aesop-Skin-Mandarin-Facial-Hydrating-Cream-60mL-large.png';
-import Logo2 from '../assets/images/for_all_products/Aesop-Skin-Fabulous-Face-Oil-25mL-Large-684x668px.png';
-import Logo3 from '../assets/images/for_all_products/Aesop-SkinParsley-Seed-Facial-Cleanser-100mL-large.png';
-import Logo4 from '../assets/images/for_all_products/Aesop-Skin-In-Two-Minds-Facial-Cleanser-200mL-Large-835x962px.png';
-
+import Logo from "../assets/images/for_all_products/Aesop-SkinParsley-Seed-Facial-Cleanser-100mL-large.png";
+import Logo1 from "../assets/images/for_all_products/Aesop-Skin-Mandarin-Facial-Hydrating-Cream-60mL-large.png";
+import Logo2 from "../assets/images/for_all_products/Aesop-Skin-Fabulous-Face-Oil-25mL-Large-684x668px.png";
+import Logo3 from "../assets/images/for_all_products/Aesop-SkinParsley-Seed-Facial-Cleanser-100mL-large.png";
+import Logo4 from "../assets/images/for_all_products/Aesop-Skin-In-Two-Minds-Facial-Cleanser-200mL-Large-835x962px.png";
 
 const responsive = {
   desktop: {
@@ -43,44 +42,47 @@ const responsive = {
   dotListClass="custom-dot-list-style"
   itemClass="carousel-item-padding-40-px"
 >
-<div>
+  <div>
     <h5>Nurturing formulations</h5>
     <h2>For all ages, genders and skin types</h2>
-    <p>Healthy skin is the roduct of careful cleansing, 
-        nourishing hydration and protection against environmental factors</p>
+    <p>
+      Healthy skin is the roduct of careful cleansing, nourishing hydration and
+      protection against environmental factors
+    </p>
   </div>
   <div>
-    <img src="" alt="Parsley Seed Anti-Oxidant Serum" />
+    <img className="" src={Logo} alt="Parsley Seed Anti-Oxidant Serum" />
     <h2>Parsley Seed Anti-Oxidant Serum</h2>
     <p>Concentrated serum in an Aloe Vera base</p>
   </div>
 
   <div>
-    <img src="" alt="Mandarin Facial Hydrating Cream" />
+    <img className="" src={Logo1} alt="Mandarin Facial Hydrating Cream" />
     <h2>Mandarin Facial Hydrating Cream</h2>
     <p>Rapidly absorbed, lightly hydrating</p>
   </div>
 
   <div>
-    <img src="" alt="Fabulous Face Oil" />
+    <img className="" src={Logo2} alt="Fabulous Face Oil" />
     <h2>Fabulous Face Oil</h2>
     <p>Botanically based hydration boost</p>
   </div>
 
   <div>
-    <img src="" alt="Parsely Seed Facial Cleanser" />
+    <img className="" src={Logo3} alt="Parsely Seed Facial Cleanser" />
     <h2>Parsely Seed Facial Cleanser</h2>
     <p>For those in polluted urban environments</p>
   </div>
 
   <div>
-    <img src="" alt="BandTea Balancing Toner" />
+    <img className="" src={Logo4} alt="BandTea Balancing Toner" />
     <h2>B and Tea Balancing Toner</h2>
     <p>Particularly suited to sensitive skin</p>
   </div>
 
-  <div>
-    <img src="" alt="Fabulous Face Cleanser" />
+  {/* <div>
+  <img className=""
+      src={Logo5} alt="Fabulous Face Cleanser" />
     <h2>Fabulous Face Cleanser</h2>
     <p>Mild Olive-derived cleansing gel</p>
   </div>
@@ -89,7 +91,7 @@ const responsive = {
     <img src="" alt="Bitter Orange Astringent Toner" />
     <h2>Bitter Orange Astringent Toner</h2>
     <p>Removes excess sebum from the skin</p>
-  </div>
+  </div> */}
 </Carousel>;
 
 export default NurturingFormulations;
