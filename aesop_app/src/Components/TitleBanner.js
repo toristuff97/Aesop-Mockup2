b294684aa2319376368ced42c186dbe4e859a190
Omari Brown <omaris.brown@gmail.com>
2020-07-21 08:19:37 -0400

    Revert "added styles component folder and moved title/header files inside"
    This reverts commit 77fd47849033ecad5c6827236dfb685968263c53.

--- aesop_app/src/Components/TitleBanner.js
+++ aesop_app/src/Components/TitleBanner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Styles/TitleBanner.css'
+import './TitleBanner.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
