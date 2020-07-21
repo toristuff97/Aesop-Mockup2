a0b16cf53380d5dc133df32843ae606c56dea478
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-07-21 19:43:11 -0400

    fixed indentation on Fragrance and updated HandsAndBody to use a captioned carousel

--- aesop_app/src/Components/Fragrance.js
+++ aesop_app/src/Components/Fragrance.js
@@ -22,62 +22,62 @@ export default class Fragrance extends Component {
     </div>
     <div class="col-sm">
     <div id="fragranceCarousel" className="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#fragranceCarousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#fragranceCarousel" data-slide-to="1"></li>
-                    <li data-target="#fragranceCarousel" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                    <img src={roomspray} class="d-block w-100" alt="Istros Aromatique Room Spray"/>
-                    <br/>
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Istros Aromatique Room Spray</h5>
-                        <p>Enlivening florals; smoky Tobacco</p>
-                    </div>
-                    </div>
-                    <div class="carousel-item">
-                    <img src={tacit} class="d-block w-100" alt="Tacit Eau de Parfum"/>
-                    <br/>
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Tacit Eau de Parfum</h5>
-                        <p>Basil Grand Vert, fresh citrus notes</p>
-                    </div>
-                    </div>
-                    <div class="carousel-item">
-                    <img src={isabelle} class="d-block w-100" alt="Isabelle Oil Burner Blend"/>
-                    <br/>
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Isabelle Oil Burner Blend</h5>
-                        <p>Stimulating scents of Spearmint, Rose</p>
-                    </div>
-                    </div>
-                    <div class="carousel-item">
-                    <img src={marrakech} class="d-block w-100" alt="Marrakech Intense Parfum"/>
-                    <br/>
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Marrakech Intense Parfum</h5>
-                        <p>Woody, spicy notes; fresh florals</p>
-                    </div>
-                    </div>
-                    <div class="carousel-item">
-                    <img src={hwyl} class="d-block w-100" alt="Hwyl Eau de Parfum"/>
-                    <br/>
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Hwyl Eau de Parfum</h5>
-                        <p>Dark, earthy Cypress with notes of Vetiver</p>
-                    </div>
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#fragranceCarousel" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#fragranceCarousel" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-                </div>
+        <ol class="carousel-indicators">
+            <li data-target="#fragranceCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#fragranceCarousel" data-slide-to="1"></li>
+            <li data-target="#fragranceCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src={roomspray} class="d-block w-100" alt="Istros Aromatique Room Spray"/>
+            <br/>
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Istros Aromatique Room Spray</h5>
+                <p>Enlivening florals; smoky Tobacco</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src={tacit} class="d-block w-100" alt="Tacit Eau de Parfum"/>
+            <br/>
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Tacit Eau de Parfum</h5>
+                <p>Basil Grand Vert, fresh citrus notes</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src={isabelle} class="d-block w-100" alt="Isabelle Oil Burner Blend"/>
+            <br/>
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Isabelle Oil Burner Blend</h5>
+                <p>Stimulating scents of Spearmint, Rose</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src={marrakech} class="d-block w-100" alt="Marrakech Intense Parfum"/>
+            <br/>
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Marrakech Intense Parfum</h5>
+                <p>Woody, spicy notes; fresh florals</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src={hwyl} class="d-block w-100" alt="Hwyl Eau de Parfum"/>
+            <br/>
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Hwyl Eau de Parfum</h5>
+                <p>Dark, earthy Cypress with notes of Vetiver</p>
+            </div>
+        </div>
+    </div>
+        <a class="carousel-control-prev" href="#fragranceCarousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#fragranceCarousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        </div>
     </div>
   </div>
 </div>
