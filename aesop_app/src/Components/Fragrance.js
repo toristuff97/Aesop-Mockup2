9f80b186cb41d8b472b5ca2dfc2b674b3e8162d6
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-07-21 20:49:44 -0400

    changed carousel-caption to bsPrefix in Fragrance

--- aesop_app/src/Components/Fragrance.js
+++ aesop_app/src/Components/Fragrance.js
@@ -31,7 +31,7 @@ export default class Fragrance extends Component {
                     <div class="carousel-item active">
                     <img src={roomspray} class="d-block w-100" alt="Istros Aromatique Room Spray"/>
                     <br/>
-                    <div class="carousel-caption d-none d-md-block">
+                    <div class="bsPrefix d-none d-md-block">
                         <h5>Istros Aromatique Room Spray</h5>
                         <p>Enlivening florals; smoky Tobacco</p>
                     </div>
@@ -39,7 +39,7 @@ export default class Fragrance extends Component {
                     <div class="carousel-item">
                     <img src={tacit} class="d-block w-100" alt="Tacit Eau de Parfum"/>
                     <br/>
-                    <div class="carousel-caption d-none d-md-block">
+                    <div class="bsPrefix d-none d-md-block">
                         <h5>Tacit Eau de Parfum</h5>
                         <p>Basil Grand Vert, fresh citrus notes</p>
                     </div>
@@ -47,7 +47,7 @@ export default class Fragrance extends Component {
                     <div class="carousel-item">
                     <img src={isabelle} class="d-block w-100" alt="Isabelle Oil Burner Blend"/>
                     <br/>
-                    <div class="carousel-caption d-none d-md-block">
+                    <div class="bsPrefix d-none d-md-block">
                         <h5>Isabelle Oil Burner Blend</h5>
                         <p>Stimulating scents of Spearmint, Rose</p>
                     </div>
@@ -55,7 +55,7 @@ export default class Fragrance extends Component {
                     <div class="carousel-item">
                     <img src={marrakech} class="d-block w-100" alt="Marrakech Intense Parfum"/>
                     <br/>
-                    <div class="carousel-caption d-none d-md-block">
+                    <div class="bsPrefix d-none d-md-block">
                         <h5>Marrakech Intense Parfum</h5>
                         <p>Woody, spicy notes; fresh florals</p>
                     </div>
@@ -63,7 +63,7 @@ export default class Fragrance extends Component {
                     <div class="carousel-item">
                     <img src={hwyl} class="d-block w-100" alt="Hwyl Eau de Parfum"/>
                     <br/>
-                    <div class="carousel-caption d-none d-md-block">
+                    <div class="bsPrefix d-none d-md-block">
                         <h5>Hwyl Eau de Parfum</h5>
                         <p>Dark, earthy Cypress with notes of Vetiver</p>
                     </div>
