deda1e1a0eb615a0e4f2068d56225e03617a4dca
Syed Shadab <syed.addan.shadab@gmail.com>
2020-07-22 01:15:02 -0400

    Reorganized imports

--- aesop_app/src/App.js
+++ aesop_app/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import './App.css';
-import TitleBanner from './Components/TitleBanner';
-import ShippingModal from './Components/ShippingModal';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Fragrance from './Components/Fragrance';
-import NurturingFormulations from './Components/NuturingFormulations';
-import ArchiAndDesign from './Components/Architecture-Design/ArchiAndDesign';
+
+import ShippingModal from './Components/ShippingModal';
+import TitleBanner from './Components/TitleBanner';
 import AesopEssentials from './Components/AesopEssentials';
+import Fragrance from './Components/Fragrance';
 import HandsAndBody from './Components/HandsAndBody';
+import ArchiAndDesign from './Components/Architecture-Design/ArchiAndDesign';
+import NurturingFormulations from './Components/NuturingFormulations';
+import Discover from './Components/Discover'; 
 import Footer from './Components/Footer';
 
 
 function App() {
   return (
     <div className="App">
-      <TitleBanner />
       <ShippingModal />
+      <TitleBanner />
       <AesopEssentials />
+      {/* GiftKits Component Goes Here */}
       <Fragrance />
+      {/* AllGifts Component Goes Here */}
       <HandsAndBody />
       <ArchiAndDesign />
       <NurturingFormulations />
+      <Discover />
+      {/* SubFooter Component Goes Here */}
       <Footer />
     </div>
   );
